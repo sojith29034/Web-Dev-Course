a5568823a9753a59a965193c5ff390f31d2c59fe
Sojith Sunny <sojithsunny29034@gmail.com>
2024-05-07 00:36:01 +0530

    drum kit project added

---
+++ Drum Kit/index.js
@@ -0,0 +1,61 @@
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numberOfDrums; i++){
+    // button clicks
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+        var drumButton = this.innerHTML;
+        sound(drumButton);
+        buttonAnimate(drumButton);
+    });
+}
+
+// keyboard clicks
+document.addEventListener("keydown",function(event){
+    var drumButton = event.key;
+    sound(drumButton);
+    buttonAnimate(drumButton);
+});
+
+function sound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+
+function buttonAnimate(drumButton){
+    var activeButton = document.querySelector("."+drumButton);
+    activeButton.classList.add("pressed");
+    setTimeout(() =>{activeButton.classList.remove("pressed")}, 50)
+};
