535810daad9b4aee082115f5a1e170d9122da927
Sojith Sunny <sojithsunny29034@gmail.com>
2024-05-06 23:29:25 +0530

    drum project added

--- JS Projects/script.js
+++ JS Projects/script.js
@@ -1,45 +1,31 @@
-// BMI Calculator
-function bmiCalculator (weight, height) {
-    var bmiValue = Math.round(weight / (height*height));
-    
-    if(bmiValue<18.5){
-        var interpretation = "Your BMI is "+bmiValue+", so you are underweight.";
-    }
-    if(bmiValue>18.5 && bmiValue<=24.9){
-        var interpretation = "Your BMI is "+bmiValue+", so you have a normal weight.";
-    }
-    if(bmiValue>24.9){
-        var interpretation = "Your BMI is "+bmiValue+", so you are overweight.";
-    }
-    
-    return interpretation;
-}
-
-/* ---------------------------------------------------------------------------------------------------------------------------------------- */
-
 // Leap Year
-function isLeap(year) { 
+function isLeapM1(year) { 
+    year = parseInt(document.querySelector(year).value);
+    var message;
+    
     if(year%4===0){
         if(year%400===0){
-            console.log("Leap year.");
+            message = year + " is a Leap Year.";
         }
         else{
             if(year%100===0){
-                console.log("Not leap year.");
+                message = year + " is not a Leap Year.";
             }
             else{
-                console.log("Leap year.");
+                message = year + " is a Leap Year.";
             }
         }
     }else{
-        console.log("Not leap year.");
+        message = year + " is not a Leap Year.";
     }
+
+    document.querySelector("#message").innerHTML=message;
 }
 
 // OR
 
 // Leap Year
-function isLeap(year) { 
+function isLeapM2(year) { 
     if(year%4===0){
         if(year%100===0){
             if(year%400===0){
@@ -61,12 +47,14 @@ function isLeap(year) {
 
 // Fibonacci Series
 function fibonacciGenerator (n) {
+    n = parseInt(document.querySelector(n).value);
     var a=[];
+
     if(n===1){
         a=[0];
     }
     else if(n===2){
-        a=[0,1];
+        a=[0, 1];
     }
     else if(n>2){
         a[0]=0;
@@ -76,9 +64,9 @@ function fibonacciGenerator (n) {
         }
     }
     else{
-        console.log("Enter a valid number.");
+        document.querySelector("#answer").innerHTML="Invalid number.";
     }
-    return a;
+    document.querySelector("#answer").innerHTML="Series: " +a;
 }
     
     
