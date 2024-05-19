e483a9648836204c3d4ca829d3c9e7f8f67486e4
Sojith Sunny <sojithsunny29034@gmail.com>
2024-05-19 13:59:22 +0530

    qr generator

--- QR Generator/index.js
+++ QR Generator/index.js
@@ -1,4 +1,7 @@
 import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+// var qr = require('qr-image');
 
 const questions = [
   {
@@ -8,12 +11,18 @@ const questions = [
   },
 ];
 
+var n = 0;
+
 // method 1 - output: The entered link is: https://semp2324.000webhostapp.com/
-inquirer.prompt(questions).then(answers => {
-  console.log(`The entered link is: ${answers.link}`);
+inquirer.prompt(questions).then((answers) => {
+  const url = answers.link;
+  var qr_png = qr.image(url);
+  qr_png.pipe(fs.createWriteStream('qrImg.png')); 
+
+  var text = ++n + ". " + url + "\n";
+  fs.appendFile("urls.txt", text, (err) => {
+    if (err) throw err;
+    console.log("File saved.");
+  })
 });
 
-// method 2 - output: The entered link is:  { link: 'https://semp2324.000webhostapp.com/' }
-inquirer.prompt(questions).then(answers => {
-  console.log(`The entered link is: `,(answers));
-});
