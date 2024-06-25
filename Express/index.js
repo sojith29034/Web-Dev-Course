ec71e2f3ea4e6df094ddee6285e3c0feb9c701cc
Sojith Sunny <sojithsunny29034@gmail.com>
2024-06-25 13:50:38 +0530

    added files

--- Express/index.js
+++ Express/index.js
@@ -1,33 +1,38 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // GET function
 app.get("/", (req, res) => {
     res.send("<h1>Hello, World!</h1>");
     // console.log(req.rawHeaders);
-})
+});
 app.get("/about", (req, res) => {
     res.send("<h1>Hello, World! I am Sojith.</h1>");
-})
+});
 app.get("/contact", (req, res) => {
     res.send("<h1>Hello, World! Contact me now.</h1>");
-})
+});
+
 
 app.post("/register", (req, res) => {
     res.sendStatus(201);
-})
+});
+
 app.put("/user/sojith", (req, res) => {
     res.sendStatus(200);
-})
+});
+
 app.patch("/user/sojith", (req, res) => {
     res.sendStatus(200);
-})
+});
+
 app.delete("/user/sojith", (req, res) => {
     res.sendStatus(200);
-})
+});
+
 
 // LISTENING
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}.`)
-})
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
